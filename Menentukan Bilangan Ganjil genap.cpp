d32e386543d99af482a5bdbffcb1e41e0ea43e94
Gigin <50097669+Gigin22@users.noreply.github.com>
2019-04-30 13:54:43 +0700

    Add files via upload

---
+++ Menentukan Bilangan Ganjil genap.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+int main ()
+{
+    int a;
+    cout<<"Maasukkan bilangan = ";
+    cin>>a;
+
+    if (a%2==0)
+    {
+        cout<<"Bilangan Genap";
+    }
+    else
+    {
+        cout<<"Bilangan Ganjil";
+    }
+}
